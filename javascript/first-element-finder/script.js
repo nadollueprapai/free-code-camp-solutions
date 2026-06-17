e9fe8e2d23545cde55a5e9f0d918098b5853260b
Nadol Lueprapai <nadollueprapai@gmail.com>
2026-06-17 22:43:42 +0000

    Added First Element Finder.

---
+++ javascript/first-element-finder/script.js
@@ -0,0 +1,15 @@
+function findElement(array, givenFunction) {
+  // Iterate through the array.
+  for (let index = 0; index < array.length; index++) {
+    // Index the element.
+    let indexedElement = array[index];
+
+    // Check if the element passes the function.
+    if (givenFunction(indexedElement)) {
+      // Return the element.
+      return indexedElement;
+    }
+  }
+}
+
+console.log(findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }))
