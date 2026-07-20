ba8af12255129a67bcbeb8a6d851f7fb8283f07a
Nadol Lueprapai <nadollueprapai@gmail.com>
2026-07-20 09:16:31 +0000

    Updated Note-taking App with localStorage.

--- javascript/javascript-and-accessibility/note-taking-app/script.js
+++ javascript/javascript-and-accessibility/note-taking-app/script.js
@@ -1,25 +1,64 @@
 const noteEl = document.getElementById("note");
 const statusEl = document.getElementById("status");
 
-let currentContent = "";
+// Keep track of previous content to accurately update status text.
+let previousContent = "";
 
+// Define a storage keyword to access the local storage data.
+const storageKey = "note";
+
+// Define a function to save the note and show a confirmation message.
+function saveNote() {
+    // Save the note text to the storage key.
+    localStorage.setItem(storageKey, noteEl.innerText);
+
+    // Show a confirmation message.
+    statusEl.textContent = "Note saved successfully!";
+}
+
+// Define a function to get the saved data.
+function getSavedNote() {
+    const savedNote = localStorage.getItem(storageKey);
+    return savedNote?.trim() || "";
+}
+
+// Triggers when user starts editing the note.
 noteEl.addEventListener("focus", () => {
-    statusEl.textContent = "";
+    // Update the text of the status element.
+    statusEl.textContent = "Editing note...";
 })
 
 noteEl.addEventListener("blur", () => {
-    const newContent = noteEl.innerHTML;
+    // Start by clearing the status text.
+    statusEl.textContent = "";
 
-    if (currentContent === newContent) {
+    // Access the current content of the note.
+    const currentContent = noteEl.innerText;
+
+    // Check if the note hasn't changed by comparing currentContent with newContent.
+    if (previousContent === currentContent) {
+        // If it is the exact same, then avoid unnecessary saving.
         return;
     }
 
-    currentContent = newContent;
-    console.log(currentContent);
+    // Update previous content to the current content of the note.
+    previousContent = currentContent;
 
-    statusEl.textContent = "Note saved successfully!";
+    // Save the note to local storage and display save confirmation message.
+    saveNote();
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-    currentContent = noteEl.textContent;
+    // Load in the note from localStorage.
+    const savedNote = getSavedNote();
+
+    // Check if the note is existent.
+    if (savedNote) {
+        // Load in the note.
+        noteEl.textContent = savedNote;
+        previousContent = savedNote;
+    } else {
+        // Set previousContent to default text.
+        previousContent = noteEl.innerText;
+    }
 });
