659dc1b98ed4cee2c1671dd34d95c0eab609aa3b
Nadol Lueprapai <nadollueprapai@gmail.com>
2026-09-12 03:36:56 +0000

    Setup React App.

---
+++ front-end-development/react-app/src/App.jsx
@@ -0,0 +1,7 @@
+import CurrentExercise from '/workspaces/free-code-camp-solutions/front-end-development/react-fundamentals/mood-board/index.jsx'
+
+function App() {
+  return <CurrentExercise/>
+}
+
+export default App
