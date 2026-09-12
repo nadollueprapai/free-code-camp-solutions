c4a5592c4a48f54aff271f419b8ab3996e91a060
Nadol Lueprapai <nadollueprapai@gmail.com>
2026-09-12 02:23:58 +0000

    Started Front End development

---
+++ front-end-development/react-fundamentals/reuseable-footer/index.jsx
@@ -0,0 +1,22 @@
+export function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+      <p>© Nadol Lueprapai 2026</p>
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </footer>
+  );
+}
